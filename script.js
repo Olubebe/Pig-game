0cb3ed08054cac0d1b86c3c414422c5ab09a94e3
Faith Oluwibe <faitholuwibe@gmail.com>
2022-06-06 10:57:08 +0100

    initial commit

--- script.js
+++ script.js
@@ -12,14 +12,27 @@ const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
 
 //Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden'); 
+let scores, currentScore, activePlayer, playing;
 
-let scores = [0, 0]
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    diceEl.classList.add('hidden'); 
+    scores = [0, 0]
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    player0El.classList.remove('player--winner')
+    player1El.classList.remove('player--winner')
+    player1El.classList.add('player--active')
+    player1El.classList.remove('player--active')
+}
+
+init()
 
 const switchPlayer = function() {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -30,15 +43,13 @@ const switchPlayer = function() {
 }
 
 btnRoll.addEventListener('click', function() {
-
-    if (playing === true) {
-
-   
+    if (playing) {
     //generationg a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
    console.log(dice);
 
    //display dice
+   
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
 
@@ -55,21 +66,24 @@ btnRoll.addEventListener('click', function() {
 
 btnHold.addEventListener('click', function() {
     // add current score to active player's score
+    if (playing) {
     scores[activePlayer] += currentScore;
    
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
     //check if the player score is >=100
-    if(scores[activePlayer] >= 20) {
+    if(scores[activePlayer] >= 100) {
         playing = false;
         diceEl.classList.add('hidden');
         document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
         document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
-    }else {
-        //switch to the next player
+    } else {
+        //switch to the next player  
    switchPlayer()
     }
-
+}
 });
+
+btnNew.addEventListener('click', init )
 
 
